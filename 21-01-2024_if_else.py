fe1a5205710baad22e7d5bbf8c92896502cca700
Sourneshwaran <157293837+Sourneshwaran@users.noreply.github.com>
2024-01-22 21:44:13 +0530

    21-01-2024_if_else

---
+++ 21-01-2024_if_else.py
@@ -0,0 +1,53 @@
+"""Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Explanation 0
+
+
+ is odd and odd numbers are weird, so print Weird.
+
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+Explanation 1
+
+
+ and  is even, so it is not weird.
+"""
+
+
+n = int(input().strip())
+
+# Conditional actions
+if n % 2 == 1:  # If n is odd
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:  # If n is even and in the inclusive range of 2 to 5
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:  # If n is even and in the inclusive range of 6 to 20
+    print("Weird")
+elif n % 2 == 0 and n > 20:  # If n is even and greater than 20
+    print("Not Weird")
+
